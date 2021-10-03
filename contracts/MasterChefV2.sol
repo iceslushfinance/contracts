2bc8e23fbb331c0ec93dca2613a9cad9fb809e2f
anil <anil@anil.com>
2021-10-03 21:00:34 +0300

    fixed contracts

--- contracts/MasterChefV2.sol
+++ contracts/MasterChefV2.sol
@@ -32,7 +32,7 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
         //   pending reward = (user.amount * pool.accTokenPerShare) - user.rewardDebt
         //
         // Whenever a user deposits or withdraws LP tokens to a pool. Here's what happens:
-        //   1. The pool's `accTokenPerShare` (and `lastRewardBlock`) gets updated.
+        //   1. The pool's `accTokenPerShare` (and `lastRewardTime`) gets updated.
         //   2. User receives the pending reward sent to his/her address.
         //   3. User's `amount` gets updated.
         //   4. User's `rewardDebt` gets updated.
@@ -41,8 +41,8 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
     // Info of each pool.
     struct PoolInfo {
         IBEP20 lpToken;           // Address of LP token contract.
-        uint256 allocPoint;       // How many allocation points assigned to this pool. Tokens to distribute per block.
-        uint256 lastRewardBlock;  // Last block number that Tokens distribution occurs.
+        uint256 allocPoint;       // How many allocation points assigned to this pool. Tokens to distribute per second.
+        uint256 lastRewardTime;  // Last timestamp that Tokens distribution occurs.
         uint256 accTokenPerShare;   // Accumulated Tokens per share, times 1e12. See below.
         uint16 withdrawFeeBP;      // Deposit fee in basis points
     }
@@ -53,10 +53,8 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
     uint256 public maxEmissionRate = 1 ether;
     // Dev address.
     address public devaddr;
-    // Token tokens created per block.
-    uint256 public tokenPerBlock;
-    // Bonus muliplier for early token makers.
-    uint256 public constant BONUS_MULTIPLIER = 1;
+    // Token tokens created per second.
+    uint256 public tokenPerSecond;
     // Deposit Fee address
     address public feeAddress;
 
@@ -66,28 +64,33 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
     mapping(uint256 => mapping(address => UserInfo)) public userInfo;
     // Total allocation points. Must be the sum of all allocation points in all pools.
     uint256 public totalAllocPoint = 0;
-    // The block number when Token mining starts.
-    uint256 public startBlock;
+    // The timestamp when Token mining starts.
+    uint256 public startTimestamp;
 
     event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
     event Withdraw(address indexed user, uint256 indexed pid, uint256 amount);
     event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount);
     event SetFeeAddress(address indexed user, address indexed newAddress);
     event SetDevAddress(address indexed user, address indexed newAddress);
-    event UpdateEmissionRate(address indexed user, uint256 goosePerBlock);
+    event UpdateEmissionRate(address indexed user, uint256 tokenPerSecond);
+    event AddPool(uint256 pid, address lp, uint256 allocPoint, uint256 fee);
+    event SetPool(uint256 pid, uint256 allocPoint, uint256 fee);
+    event SetStartTime(uint256 startTime);
 
     constructor(
         Token _token,
         address _devaddr,
         address _feeAddress,
-        uint256 _tokenPerBlock,
-        uint256 _startBlock
+        uint256 _tokenPerSecond,
+        uint256 _startTimestamp
     ) {
+        require(_feeAddress != address(0), "no zero address");
+        require(_devaddr != address(0), "no zero address");
         token = _token;
         devaddr = _devaddr;
         feeAddress = _feeAddress;
-        tokenPerBlock = _tokenPerBlock;
-        startBlock = _startBlock;
+        tokenPerSecond = _tokenPerSecond;
+        startTimestamp = _startTimestamp;
     }
 
     function poolLength() external view returns (uint256) {
@@ -106,32 +109,39 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
         if (_withUpdate) {
             massUpdatePools();
         }
-        uint256 lastRewardBlock = block.number > startBlock ? block.number : startBlock;
+        // balanceOf check
+        // this should be works fine if address is a token
+        uint256 balance = _lpToken.balanceOf(address(this));
+
+        uint256 lastRewardTime = block.timestamp > startTimestamp ? block.timestamp : startTimestamp;
         totalAllocPoint = totalAllocPoint.add(_allocPoint);
         poolExistence[_lpToken] = true;
         poolInfo.push(PoolInfo({
         lpToken : _lpToken,
         allocPoint : _allocPoint,
-        lastRewardBlock : lastRewardBlock,
+        lastRewardTime : lastRewardTime,
         accTokenPerShare : 0,
         withdrawFeeBP : _withdrawFeeBP
         }));
+
+        emit AddPool(poolInfo.length.sub(1), address(_lpToken), _allocPoint, _withdrawFeeBP);
     }
 
     // Update the given pool's Token allocation point and deposit fee. Can only be called by the owner.
     function set(uint256 _pid, uint256 _allocPoint, uint16 _withdrawFeeBP, bool _withUpdate) public onlyOwner {
-        require(_withdrawFeeBP <= 300, "add: invalid withdrawal fee basis points");
+        require(_withdrawFeeBP <= 400, "add: invalid withdrawal fee basis points");
         if (_withUpdate) {
             massUpdatePools();
         }
         totalAllocPoint = totalAllocPoint.sub(poolInfo[_pid].allocPoint).add(_allocPoint);
         poolInfo[_pid].allocPoint = _allocPoint;
         poolInfo[_pid].withdrawFeeBP = _withdrawFeeBP;
+        emit SetPool(_pid, _allocPoint, _withdrawFeeBP);
     }
 
     // Return reward multiplier over the given _from to _to block.
     function getMultiplier(uint256 _from, uint256 _to) public view returns (uint256) {
-        return _to.sub(_from).mul(BONUS_MULTIPLIER);
+        return _to.sub(_from);
     }
 
     // View function to see pending Tokens on frontend.
@@ -140,9 +150,12 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
         UserInfo storage user = userInfo[_pid][_user];
         uint256 accTokenPerShare = pool.accTokenPerShare;
         uint256 lpSupply = pool.lpToken.balanceOf(address(this));
-        if (block.number > pool.lastRewardBlock && lpSupply != 0) {
-            uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
-            uint256 tokenReward = multiplier.mul(tokenPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+        // CHANGED
+        if (lpSupply == 0 || totalAllocPoint == 0) return 0;
+
+        if (block.timestamp > pool.lastRewardTime && lpSupply != 0) {
+            uint256 multiplier = getMultiplier(pool.lastRewardTime, block.timestamp);
+            uint256 tokenReward = multiplier.mul(tokenPerSecond).mul(pool.allocPoint).div(totalAllocPoint);
             accTokenPerShare = accTokenPerShare.add(tokenReward.mul(1e12).div(lpSupply));
         }
         return user.amount.mul(accTokenPerShare).div(1e12).sub(user.rewardDebt);
@@ -159,29 +172,30 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
     // Update reward variables of the given pool to be up-to-date.
     function updatePool(uint256 _pid) public {
         PoolInfo storage pool = poolInfo[_pid];
-        if (block.number <= pool.lastRewardBlock) {
+        if (block.timestamp <= pool.lastRewardTime) {
             return;
         }
         uint256 lpSupply = pool.lpToken.balanceOf(address(this));
         if (lpSupply == 0 || pool.allocPoint == 0) {
-            pool.lastRewardBlock = block.number;
+            pool.lastRewardTime = block.timestamp;
             return;
         }
-        uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
-        uint256 tokenReward = multiplier.mul(tokenPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+        uint256 multiplier = getMultiplier(pool.lastRewardTime, block.timestamp);
+        uint256 tokenReward = multiplier.mul(tokenPerSecond).mul(pool.allocPoint).div(totalAllocPoint);
         token.mint(devaddr, tokenReward.div(10));
         token.mint(address(this), tokenReward);
         uint256 burnAmount = tokenReward.mul(2).div(100);
 
-        token.transfer(0x000000000000000000000000000000000000dEaD, burnAmount);
+        safeTokenTransfer(0x000000000000000000000000000000000000dEaD, burnAmount);
+
         tokenReward = tokenReward.sub(burnAmount);
 
         pool.accTokenPerShare = pool.accTokenPerShare.add(tokenReward.mul(1e12).div(lpSupply));
-        pool.lastRewardBlock = block.number;
+        pool.lastRewardTime = block.timestamp;
     }
 
     // Deposit LP tokens to MasterChef for Token allocation.
-    function deposit(uint256 _pid, uint256 _amount) public nonReentrant {
+    function deposit(uint256 _pid, uint256 _amount) external nonReentrant {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
         updatePool(_pid);
@@ -202,7 +216,7 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
     }
 
     // Withdraw LP tokens from MasterChef.
-    function withdraw(uint256 _pid, uint256 _amount) public nonReentrant {
+    function withdraw(uint256 _pid, uint256 _amount) external nonReentrant {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
         require(user.amount >= _amount, "withdraw: not good");
@@ -215,7 +229,8 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
             user.amount = user.amount.sub(_amount);
             uint256 fee = 0;
             if (pool.withdrawFeeBP > 0) {
-                fee = _amount.mul(pool.withdrawFeeBP).div(10000);
+                // Changed
+                fee = _amount.mul(pool.withdrawFeeBP).div(100);
                 pool.lpToken.safeTransfer(feeAddress, fee);
             }
             pool.lpToken.safeTransfer(address(msg.sender), _amount.sub(fee));
@@ -225,7 +240,7 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
     }
 
     // Withdraw without caring about rewards. EMERGENCY ONLY.
-    function emergencyWithdraw(uint256 _pid) public nonReentrant {
+    function emergencyWithdraw(uint256 _pid) external nonReentrant {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
         uint256 fee = 0;
@@ -233,11 +248,12 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
         user.amount = 0;
         user.rewardDebt = 0;
         if (pool.withdrawFeeBP > 0) {
-            fee = user.amount.mul(pool.withdrawFeeBP).div(10000);
+            // changed
+            fee = amount.mul(pool.withdrawFeeBP).div(10000);
             amount = amount.sub(fee);
             pool.lpToken.safeTransfer(feeAddress, fee);
         }
-        pool.lpToken.safeTransfer(address(msg.sender), amount);
+        pool.lpToken.safeTransfer(msg.sender, amount);
         emit EmergencyWithdraw(msg.sender, _pid, amount);
     }
 
@@ -254,31 +270,37 @@ contract MasterChefV2 is Ownable, ReentrancyGuard {
     }
 
     // Update dev address by the previous dev.
-    function dev(address _devaddr) public {
+    function setDevAddress(address _devaddr) external {
         require(msg.sender == devaddr, "dev: wut?");
+        require(_devaddr != address(0), "no zero address");
         devaddr = _devaddr;
         emit SetDevAddress(msg.sender, _devaddr);
     }
 
-    function setFeeAddress(address _feeAddress) public {
+    function setFeeAddress(address _feeAddress) external {
         require(msg.sender == feeAddress, "setFeeAddress: FORBIDDEN");
+        require(_feeAddress != address(0), "no zero address");
         feeAddress = _feeAddress;
         emit SetFeeAddress(msg.sender, _feeAddress);
     }
 
     //Pancake has to add hidden dummy pools inorder to alter the emission, here we make it simple and transparent to all.
-    function updateEmissionRate(uint256 _tokenPerBlock) public onlyOwner {
+    function updateEmissionRate(uint256 _tokenPerSecond) external onlyOwner {
+        require(_tokenPerSecond <= 1 ether, "Maximum exceeded");
         massUpdatePools();
-        tokenPerBlock = _tokenPerBlock;
-        emit UpdateEmissionRate(msg.sender, _tokenPerBlock);
+        tokenPerSecond = _tokenPerSecond;
+        emit UpdateEmissionRate(msg.sender, _tokenPerSecond);
     }
 
-    function setStartBlock(uint256 _startBlock) external onlyOwner {
-        require(block.number < startBlock, "It's too late to postpone mining. It has already started");
-        startBlock = _startBlock;
+    function setStartTime(uint256 _startTimestamp) external onlyOwner {
+        require(block.timestamp < startTimestamp, "It's too late to postpone mining. It has already started");
+        require(_startTimestamp > startTimestamp, "Cannot set to past timestamp");
+        startTimestamp = _startTimestamp;
         uint256 length = poolInfo.length;
         for (uint256 pid = 0; pid < length; ++pid) {
-            poolInfo[pid].lastRewardBlock = startBlock;
+            poolInfo[pid].lastRewardTime = _startTimestamp;
         }
+
+        emit SetStartTime(_startTimestamp);
     }
 }
